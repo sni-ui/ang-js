32a36c6d7d0744055a886951f0bc087b4ff79457
Snigdha Reddy <snigdha24reddy@gmail.com>
2016-09-28 18:04:23 -0400

    3-scope

---
+++ 3-bind-$scope/js/MainController.js
@@ -0,0 +1,9 @@
+var MainController = function MainController($scope) {
+    var self = this;
+    self.name = 'Dixit Patel';
+    $scope.name = 'Amit';
+};
+
+angular
+    .module('app')
+    .controller('MainController', MainController);
